941bf4bbaa4e303ddbdd83f9359ce8b0460e0e4d
Katarzyna <katarzyna.pabian357@gmail.com>
2023-12-16 14:21:37 +0100

    Create car entity

---
+++ Data/Entities/CarEntity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarStack.Areas.Identity.Data;
+using CarStack.Models;
+
+namespace Data.Entities;
+
+[Table("cars")]
+public class CarEntity
+{
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string Model { get; set; }
+    [Required]
+    public int ManufacturerId { get; set; }
+    public Manufacturer Manufacturer { get; set; }
+    public double? Capacity { get; set; }
+    public int? Power { get; set; }
+    [MaxLength(50)]
+    public string? Motor { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string RegistrationNumber { get; set; }
+    [Required]
+    public Priority Priority { get; set; }
+    [Required]
+    public int CarStackUserId { get; set; }
+    [Required]
+    public CarStackUser User { get; set; }
+
+}
+
+
+
