6348329bb23ced2d0bd1e6961b22f843ac0df910
Katarzyna <katarzyna.pabian357@gmail.com>
2023-12-16 15:12:59 +0100

    Create manufacturer entity

---
+++ Data/Entities/ManufacturerEntity.cs
@@ -0,0 +1,20 @@
+﻿using CarStack.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+[Table("manufacturers")]
+public class ManufacturerEntity
+{
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+    [MaxLength(50)]
+    public string City { get; set; }
+    [MaxLength(10)]
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+    public List<CarEntity> Cars { get; set; }
+}
