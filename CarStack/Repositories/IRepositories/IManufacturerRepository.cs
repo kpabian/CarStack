131558c3f825c37368309ef5d20ec978a732eab1
Katarzyna <katarzyna.pabian357@gmail.com>
2023-12-17 18:12:58 +0100

    Create manufacturer repository

---
+++ CarStack/Repositories/IRepositories/IManufacturerRepository.cs
@@ -0,0 +1,11 @@
+﻿using CarStack.Models;
+
+namespace CarStack.Repositories.IRepositories;
+
+public interface IManufacturerRepository
+{
+    Task<IEnumerable<Manufacturer>> Get(int? manufacturerId);
+    Task Create(Manufacturer car);
+    Task Update(Manufacturer car);
+    Task Delete(int manufacturerId);
+}
---
+++ CarStack/Repositories/ManufacturerRepository.cs
@@ -0,0 +1,39 @@
+﻿using CarStack.Areas.Identity.Data;
+using CarStack.Models;
+using CarStack.Repositories.IRepositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarStack.Repositories;
+
+public sealed class ManufacturerRepository(CarDBContext context) : IManufacturerRepository
+{
+    private readonly CarDBContext _context = context;
+
+    public async Task<IEnumerable<Manufacturer>> Get(int? manufacturerId)
+    {
+        var query = _context.Manufacturers.AsQueryable();
+        if (manufacturerId != null)
+            query = query.Where(x => x.Id == manufacturerId);
+        return await query.ToListAsync();
+    }
+
+    public async Task Create(Manufacturer manufacturer)
+    {
+        await _context.Manufacturers.AddAsync(manufacturer);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Update(Manufacturer manufacturer)
+    {
+        var existingManufacturer = await _context.Manufacturers.FindAsync(manufacturer.Id);
+        _context.Entry(existingManufacturer).CurrentValues.SetValues(manufacturer);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(int manufacturerId)
+    {
+        var manufacturerToDelete = await _context.Manufacturers.FindAsync(manufacturerId);
+        _context.Manufacturers.Remove(manufacturerToDelete);
+        await _context.SaveChangesAsync();
+    }
+}
