4e1e69408ae8a9b3075e43c2d30a1e4d37d2b0b2
Katarzyna <katarzyna.pabian357@gmail.com>
2023-12-17 17:20:21 +0100

    Create car repository

---
+++ CarStack/Repositories/CarRepository.cs
@@ -0,0 +1,42 @@
+﻿using CarStack.Areas.Identity.Data;
+using CarStack.Models;
+using CarStack.Repositories.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CarStack.Repositories;
+
+public sealed class CarRepository(CarDBContext context) : ICarRepository
+{
+    private readonly CarDBContext _context = context;
+
+    public async Task<IEnumerable<Car>> Get(int? carId = null)
+    {
+        var query = _context.Cars
+            .Include(c => c.Manufacturer)
+            .AsQueryable();
+        if (carId != null)
+            query = query.Where(x => x.Id == carId);
+        return await query.ToListAsync();
+    }
+
+    public async Task Create(Car car)
+    {
+        await _context.Cars.AddAsync(car);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Update(Car car)
+    {
+        var existingCar = await _context.Cars.FindAsync(car.Id);
+        _context.Entry(existingCar).CurrentValues.SetValues(car);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(int carId)
+    {
+        var carToDelete = await _context.Cars.FindAsync(carId);
+        _context.Cars.Remove(carToDelete);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ CarStack/Repositories/IRepositories/ICarRepository.cs
@@ -0,0 +1,12 @@
+﻿using CarStack.Models;
+using System.Linq.Expressions;
+
+namespace CarStack.Repositories.IRepositories;
+
+public interface ICarRepository
+{
+    Task<IEnumerable<Car>> Get(int? carId = null);
+    Task Create(Car car);
+    Task Update(Car car);
+    Task Delete(int carId);
+}
