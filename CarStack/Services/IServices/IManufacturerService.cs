ef665613251c0c6cffd9ba28727109c1ac25b33a
Katarzyna <katarzyna.pabian357@gmail.com>
2024-01-01 14:24:22 +0100

    Create manufacturer service

---
+++ CarStack/Services/IServices/IManufacturerService.cs
@@ -0,0 +1,12 @@
+﻿using CarStack.Models;
+
+namespace CarStack.Services.IServices;
+
+public interface IManufacturerService
+{
+    Task<IEnumerable<Manufacturer>> GetAll();
+    Task<Manufacturer?> GetById(int id);
+    Task Create(Manufacturer manufacturer);
+    Task Update(Manufacturer manufacturer);
+    Task Delete(int id);
+}
---
+++ CarStack/Services/ManufacturerService.cs
@@ -0,0 +1,19 @@
+﻿using CarStack.Models;
+using CarStack.Repositories.IRepositories;
+using CarStack.Services.IServices;
+
+namespace Infrastructure.Services;
+
+public sealed class ManufacturerService(IManufacturerRepository _repo) : IManufacturerService
+{
+    public async Task<IEnumerable<Manufacturer>> GetAll() => await _repo.Get(null);
+
+    public async Task<Manufacturer?> GetById(int id) => (await _repo.Get(id)).FirstOrDefault();
+
+    public async Task Create(Manufacturer manufacturer) => await _repo.Create(manufacturer);
+
+    public async Task Update(Manufacturer manufacturer) => await _repo.Update(manufacturer);
+
+    public async Task Delete(int manufacturerId) => await _repo.Delete(manufacturerId);
+}
+
