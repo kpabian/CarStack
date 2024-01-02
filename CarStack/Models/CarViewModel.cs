061cd6e6bd5d22afbb5d6594aed01d8be1485e80
Katarzyna <katarzyna.pabian357@gmail.com>
2024-01-02 15:53:50 +0100

    Create car view model

---
+++ CarStack/Models/CarViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CarStack.Models;
+
+public sealed class CarViewModel
+{
+    public Car Car { get; set; } = new Car();
+
+    public IEnumerable<Manufacturer> Manufacturers { get; set; } = [];
+
+}
