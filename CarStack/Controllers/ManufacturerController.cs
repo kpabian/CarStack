1456b9282ffe1bcbda6fd8d690735803b41993f9
Katarzyna <katarzyna.pabian357@gmail.com>
2024-01-07 18:15:41 +0100

    Add update view to manufacturers

--- CarStack/Controllers/ManufacturerController.cs
+++ CarStack/Controllers/ManufacturerController.cs
@@ -29,6 +29,24 @@ public sealed class ManufacturerController(
     }
 
     [HttpGet]
+    public async Task<IActionResult> Update(int id)
+    {
+        var manufacturer = await manufacturerService.GetById(id);
+        if (manufacturer is null) return NotFound();
+        return View(manufacturer);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(Manufacturer manufacturer)
+    {
+        if (!ModelState.IsValid)
+            return View(manufacturer);
+
+        await manufacturerService.Update(manufacturer);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
     public async Task<IActionResult> Read(int id)
     {
         var manufacturer = await manufacturerService.GetById(id);
